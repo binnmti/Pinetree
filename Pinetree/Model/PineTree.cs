8add5f7c4387205f513a36d3e906b7188589403e
Bin Matsui <binnmtiisprogrammer@gmail.com>
2025-03-03 08:46:55 +0900

    Review Repair & Refactor

--- Pinetree/Model/PineTree.cs
+++ Pinetree/Model/PineTree.cs
@@ -3,8 +3,8 @@
 public class PineTree(long id, string title, string content, long parentId, long groupId)
 {
     public long Id { get; } = id;
-    public string Title { get; } = title;
-    public string Content { get; } = content;
+    public string Title { get; set; } = title;
+    public string Content { get; set; } = content;
     public long ParentId { get; set; } = parentId;
     public long GroupId { get; set; } = groupId;
 
---
+++ Pinetree/Model/PinneconeUpdater.cs
@@ -0,0 +1,20 @@
+﻿namespace Pinetree.Model;
+
+public static class PinneconeUpdater
+{
+
+    public static PineTree Update(this PineTree tree, long id, string title, string content)
+    {
+        if (tree.Id == id)
+        {
+            tree.Title = title;
+            tree.Content = content;
+            return tree;
+        }
+        foreach (var child in tree.Children)
+        {
+            Update(child, id, title, content);
+        }
+        return tree;
+    }
+}
