1f2e9fc777af8aa508d09e44c4842bed16dbc836
Bin Matsui <binnmtiisprogrammer@gmail.com>
2025-03-08 22:38:16 +0900

    PineTree

--- Pinetree/Components/Services/PineconeService.cs
+++ Pinetree/Components/Services/PineconeService.cs
@@ -4,31 +4,44 @@ using Pinetree.Model;
 
 namespace Pinetree.Components.Services;
 
-public class PineconeService
+public static class PineconeService
 {
-    private readonly ApplicationDbContext _dbContext;
-
-    public PineconeService(ApplicationDbContext dbContext)
-    {
-        _dbContext = dbContext;
-    }
-
-    public async Task<string> CreatePineconeAsync(string userId)
+    public static async Task<string> CreateAsync(ApplicationDbContext dbContext, string userId)
     {
         var baseTitle = "Untitled";
         var title = baseTitle;
         var counter = 1;
-        while (await _dbContext.Pinecone.AnyAsync(p => p.UserId == userId && p.Title == title && p.ParentId == null))
+        while (await dbContext.Pinecone.AnyAsync(p => p.UserId == userId && p.Title == title && p.ParentId == null))
         {
             title = $"{baseTitle} {counter}";
             counter++;
         }
         var pinecone = new Pinecone { Title = title, Content = "", GroupId = 0, ParentId = null, UserId = userId, IsSandbox = true };
-        await _dbContext.Pinecone.AddAsync(pinecone);
-        await _dbContext.SaveChangesAsync();
+        await dbContext.Pinecone.AddAsync(pinecone);
+        await dbContext.SaveChangesAsync();
         pinecone.GroupId = pinecone.Id;
-        await _dbContext.SaveChangesAsync();
+        await dbContext.SaveChangesAsync();
 
         return $"/{userId}/Edit/{pinecone.Id}";
+    }
+
+    public static async Task DeleteAsync(ApplicationDbContext dbContext, Pinecone pinecone)
+    {
+        await DeleteChildren(dbContext, pinecone);
+        dbContext.Pinecone.RemoveRange(pinecone);
+        await dbContext.SaveChangesAsync();
+    }
+
+    private static async Task DeleteChildren(ApplicationDbContext dbContext, Pinecone parent)
+    {
+        foreach (var child in parent.Children)
+        {
+            var childEntity = await dbContext.Pinecone.FirstOrDefaultAsync(p => p.Id == child.Id);
+            if (childEntity != null)
+            {
+                await DeleteChildren(dbContext, childEntity);
+                dbContext.Pinecone.Remove(childEntity);
+            }
+        }
     }
 }
