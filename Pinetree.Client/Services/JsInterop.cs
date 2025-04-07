c9f4650150996dbce6f9d49acb284a34135db9aa
Bin Matsui <binnmtiisprogrammer@gmail.com>
2025-04-08 08:06:26 +0900

    comment

--- Pinetree.Client/Services/JsInterop.cs
+++ Pinetree.Client/Services/JsInterop.cs
@@ -13,10 +13,12 @@ public static class JsInterop
     public static ValueTask<string> PromptAsync(this IJSRuntime jsRuntime, string message, string defaultValue = "")
         => jsRuntime.InvokeAsync<string>("prompt", message, defaultValue);
 
-    // TODO: eval is not good
+    // TODO: Replace eval usage with a dedicated JS function for better security and performance.
+    // See PR #15 discussion for alternative implementation approaches.
     public static ValueTask<string> GetTextAreaValueAsync(this IJSRuntime jsRuntime)
         => jsRuntime.InvokeAsync<string>("eval", $"document.querySelector('textarea').value");
 
+    // This calling method does not match the design intent of Blazor/JSInterop.
     public static ValueTask CopyToClipboardAsync(this IJSRuntime jsRuntime, string text)
-        => jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", $"{text}");
+        => jsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", text);
 }
