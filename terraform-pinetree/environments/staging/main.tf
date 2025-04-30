4a1048d7c51ffa1c38002855c47c08118a102660
Bin Matsui <binnmtiisprogrammer@gmail.com>
2025-04-30 20:59:18 +0900

    terraform

--- terraform-pinetree/environments/staging/main.tf
+++ terraform-pinetree/environments/staging/main.tf
@@ -77,6 +77,14 @@ resource "azurerm_mssql_server" "sqlserver" {
   administrator_login          = "sqladmin"
   administrator_login_password = var.administrator_login_password
   version                      = "12.0"
+  public_network_access_enabled = true
+}
+
+resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
+  name             = "AllowAzureServices"
+  server_id        = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
 
 # SQL Database
